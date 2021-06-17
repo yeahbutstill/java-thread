20d5cd3f7ca17ea3c8a77df67e9f6bf4e54d9187
yeahbutstill <mayadani28@gmail.com>
2021-06-17 12:13:43 +0700

    Synchoronization Test

--- src/test/java/net/greatsite/yeahbutstill/javathread/SynchronizationTest.java
+++ src/test/java/net/greatsite/yeahbutstill/javathread/SynchronizationTest.java
@@ -1,2 +1,33 @@
-package net.greatsite.yeahbutstill.javathread;public class SynchronizationTest {
+package net.greatsite.yeahbutstill.javathread;
+
+import org.junit.jupiter.api.Test;
+
+public class SynchronizationTest {
+
+    @Test
+    void counter() throws InterruptedException {
+
+        var counter = new SynchronizedCounter();
+        Runnable runnable = () -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter.increment();
+            }
+        };
+
+        var thread = new Thread(runnable);
+        var thread1 = new Thread(runnable);
+        var thread2 = new Thread(runnable);
+
+        thread.start();
+        thread1.start();
+        thread2.start();
+
+        thread.join();
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter.getValue());
+
+    }
+
 }
