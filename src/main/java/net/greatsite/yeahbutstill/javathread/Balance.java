504fc41ea4a797852e61fe42920544412a1ea351
yeahbutstill <mayadani28@gmail.com>
2021-06-17 13:48:12 +0700

    Contoh Deadlock

--- src/main/java/net/greatsite/yeahbutstill/javathread/Balance.java
+++ src/main/java/net/greatsite/yeahbutstill/javathread/Balance.java
@@ -1,2 +1,30 @@
-package net.greatsite.yeahbutstill.javathread;public class Balance {
+package net.greatsite.yeahbutstill.javathread;
+
+public class Balance {
+
+    private Long value;
+
+    public Balance(Long value) {
+        this.value = value;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    public static void transfer(Balance from, Balance to, Long value) throws InterruptedException {
+        synchronized (from) {
+            Thread.sleep(1_000);
+            synchronized (to) {
+                Thread.sleep(1_000);
+                from.setValue(from.getValue() - value);
+                to.setValue(to.getValue() + value);
+            }
+        }
+    }
+
 }
--- src/test/java/net/greatsite/yeahbutstill/javathread/DeadlockTest.java
+++ src/test/java/net/greatsite/yeahbutstill/javathread/DeadlockTest.java
@@ -1,2 +1,39 @@
-package net.greatsite.yeahbutstill.javathread;public class DeadlockTest {
+package net.greatsite.yeahbutstill.javathread;
+
+import org.junit.jupiter.api.Test;
+
+public class DeadlockTest {
+
+    @Test
+    void transfer() throws InterruptedException {
+
+        var balance1 = new Balance(1_000_000L);
+        var balance2 = new Balance(1_000_000L);
+
+        var thread1 = new Thread(() -> {
+            try {
+                Balance.transfer(balance1, balance2, 500_000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        var thread2 = new Thread(() -> {
+            try {
+                Balance.transfer(balance2, balance1, 500_000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(balance1.getValue());
+        System.out.println(balance2.getValue());
+
+    }
 }
