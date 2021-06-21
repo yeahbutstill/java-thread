1994759bb00dc838acb3ab15b816286d9e5d8ad7
yeahbutstill <mayadani28@gmail.com>
2021-06-21 20:58:36 +0700

    Synchoronizer Semaphore Test

--- src/test/java/net/greatsite/yeahbutstill/javathread/SemaphoreTest.java
+++ src/test/java/net/greatsite/yeahbutstill/javathread/SemaphoreTest.java
@@ -1,2 +1,36 @@
-package net.greatsite.yeahbutstill.javathread;public class SemaphoreTest {
+package net.greatsite.yeahbutstill.javathread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreTest {
+
+    @Test
+    void test() throws InterruptedException {
+
+        // Run per 20 pekerjaan yang dieksekusi
+        final var semaphore = new Semaphore(20);
+        final var executor = Executors.newFixedThreadPool(100);
+
+        for (int i = 0; i < 1000; i++) {
+            executor.execute(() -> {
+               try {
+                   // mengacquire menunggu 1000 thread sampai semaphorenya release
+                   semaphore.acquire();
+                   Thread.sleep(1000);
+                   System.out.println("Finish");
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               } finally {
+                   semaphore.release();
+               }
+            });
+        }
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+
+    }
 }
