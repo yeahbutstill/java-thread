55d443c77d68d641926f82c4242ff187194262b5
yeahbutstill <mayadani28@gmail.com>
2021-06-21 20:37:27 +0700

    Lock

--- src/main/java/net/greatsite/yeahbutstill/javathread/CounterLock.java
+++ src/main/java/net/greatsite/yeahbutstill/javathread/CounterLock.java
@@ -1,2 +1,25 @@
-package net.greatsite.yeahbutstill.javathread;public class CounterLock {
+package net.greatsite.yeahbutstill.javathread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class CounterLock {
+
+    private Long value = 0L;
+
+    final private Lock lock = new ReentrantLock();
+
+    public void increment() {
+        try {
+            lock.lock();
+            value++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
 }
