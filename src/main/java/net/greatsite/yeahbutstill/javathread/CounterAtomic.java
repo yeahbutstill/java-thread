aec8853ba0f53dafcac076abb1a0458831ddc1a0
yeahbutstill <mayadani28@gmail.com>
2021-06-21 18:46:11 +0700

    CounterAtomic

---
+++ src/main/java/net/greatsite/yeahbutstill/javathread/CounterAtomic.java
@@ -0,0 +1,17 @@
+package net.greatsite.yeahbutstill.javathread;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CounterAtomic {
+
+    private AtomicLong value = new AtomicLong(0L);
+
+    public void increment() {
+        value.incrementAndGet();
+    }
+
+    public Long getValue() {
+        return value.get();
+    }
+
+}
