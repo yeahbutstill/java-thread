cc419635094e94bbae498005c51b0d08abc3af1b
yeahbutstill <mayadani28@gmail.com>
2021-06-21 09:59:13 +0700

    ScheduledExecutor test

---
+++ src/test/java/net/greatsite/yeahbutstill/javathread/ScheduledExecutorTest.java
@@ -0,0 +1,30 @@
+package net.greatsite.yeahbutstill.javathread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorTest {
+
+    @Test
+    void delayedJob() throws InterruptedException {
+        var executor = Executors.newScheduledThreadPool(20);
+        var future = executor.schedule(() ->
+                System.out.println("Hello Scheduled"), 5, TimeUnit.SECONDS
+        );
+        System.out.println(future.getDelay(TimeUnit.SECONDS));
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+    @Test
+    void periodicJob() throws InterruptedException {
+        var executor = Executors.newScheduledThreadPool(20);
+        var future = executor.scheduleAtFixedRate(() ->
+                System.out.println("Hello Scheduled"), 2, 2, TimeUnit.SECONDS
+        );
+        System.out.println(future.getDelay(TimeUnit.SECONDS));
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+}
